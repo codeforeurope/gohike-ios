901f0e11c8ba18acf50eb0d8e51f91bd82e59a98
Giovanni Maggini <giovanni.maggini@gmail.com>
2013-05-28 11:06:35 +0200

    Deleted old models and content

--- ScavengerApp/Model/DataModels.h
+++ ScavengerApp/Model/DataModels.h
@@ -5,10 +5,4 @@
 //  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
 //
 
-#import "PlayerProgress.h"
-#import "Routes.h"
-#import "GameData.h"
-#import "Route.h"
-#import "Location.h"
 #import "Checkin.h"
-#import "Profiles.h"
