ff26bc1ef9d7d4ff161808f24bb6ba738c041046
Lodewijk Loos <lodewijk@Lodewijk-Yeah.local>
2013-05-29 16:27:24 +0200

    added back button and map button in compass and backbutton view

---
+++ ScavengerApp/BackButtonView.h
@@ -0,0 +1,13 @@
+//
+//  BackButtonView.h
+//  ScavengerApp
+//
+//  Created by Lodewijk Loos on 29-05-13.
+//  Copyright (c) 2013 Code for Europe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BackButtonView : UIView
+
+@end
