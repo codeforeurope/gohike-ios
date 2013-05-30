d1aea55a56bf79cdfd655c99a2e9fda2f2c932f0
Lodewijk Loos <lodewijk@Lodewijk-Yeah.local>
2013-05-30 11:11:23 +0200

    added generic custom bar button

---
+++ ScavengerApp/CustomBarButtonView.h
@@ -0,0 +1,15 @@
+//
+//  CustomBarButtonView.h
+//  ScavengerApp
+//
+//  Created by Lodewijk Loos on 30-05-13.
+//  Copyright (c) 2013 Code for Europe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomBarButtonView : UIView
+
+- (id)initWithFrame:(CGRect)frame imageName:(NSString*)imageName text:(NSString*)text target:(id)target action:(SEL)selector;
+
+@end
---
+++ ScavengerApp/CustomBarButtonView.m
@@ -0,0 +1,45 @@
+//
+//  CustomBarButtonView.m
+//  ScavengerApp
+//
+//  Created by Lodewijk Loos on 30-05-13.
+//  Copyright (c) 2013 Code for Europe. All rights reserved.
+//
+
+#import "CustomBarButtonView.h"
+
+@implementation CustomBarButtonView
+
+- (id)initWithFrame:(CGRect)frame imageName:(NSString*)imageName text:(NSString*)text target:(id)target action:(SEL)selector
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        //add icon vertical mid
+        UIImageView *backImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:imageName]];
+        backImage.center = CGPointMake(backImage.center.x, frame.size.height / 2);
+        [self addSubview:backImage];
+        
+        //add text vertical mid after
+        if(text)
+        {
+            NSString *labelText = NSLocalizedString(text, nil);
+            float padding = 10;
+            UIFont *font =  [UIFont fontWithName:@"HelveticaNeue" size:[UIFont systemFontSize]];
+            CGSize labelSize = [labelText sizeWithFont:font];
+            UILabel *backLabel = [[UILabel alloc] initWithFrame:CGRectMake(backImage.frame.size.width + padding, (frame.size.height - labelSize.height) / 2, labelSize.width, labelSize.height)];
+            backLabel.font = font;
+            backLabel.text = text;
+            backLabel.textColor = [UIColor whiteColor];
+            backLabel.backgroundColor = [UIColor clearColor];
+            [self addSubview:backLabel];
+        }
+            
+        //
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:target action:selector];
+        [self addGestureRecognizer:tap];
+    }
+    
+    return self;
+}
+
+@end
