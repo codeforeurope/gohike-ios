1652d9d620200fe5eecfd618bdd9480e081e27f8
Giovanni Maggini <giovanni.maggini@gmail.com>
2013-08-22 12:50:53 +0200

    added collection header

---
+++ ScavengerApp/Cells/CatalogViewCollectionHeader.h
@@ -0,0 +1,16 @@
+//
+//  CatalogViewCollectionHeader.h
+//  GoHikeAmsterdam
+//
+//  Created by Giovanni on 8/22/13.
+//  Copyright (c) 2013 Code for Europe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CatalogViewCollectionHeader : UIView
+
+@property (nonatomic, weak) IBOutlet UIImageView *headerImage;
+@property (nonatomic, weak) IBOutlet UILabel *headerLabel;
+
+@end
---
+++ ScavengerApp/Cells/CatalogViewCollectionHeader.m
@@ -0,0 +1,31 @@
+//
+//  CatalogViewCollectionHeader.m
+//  GoHikeAmsterdam
+//
+//  Created by Giovanni on 8/22/13.
+//  Copyright (c) 2013 Code for Europe. All rights reserved.
+//
+
+#import "CatalogViewCollectionHeader.h"
+
+@implementation CatalogViewCollectionHeader
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
