7ad526d93cf012a615eba453fe69f2620279c07f
Giovanni Maggini <giovanni.maggini@gmail.com>
2013-09-01 00:54:09 +0200

    added new cells for LocationDetailTableViewController

---
+++ ScavengerApp/Cells/LocationDetailPictureCell.h
@@ -0,0 +1,15 @@
+//
+//  LocationDetailPictureCell.h
+//  GoHikeAmsterdam
+//
+//  Created by Giovanni on 9/1/13.
+//  Copyright (c) 2013 Code for Europe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationDetailPictureCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UIImageView *imageView;
+
+@end
---
+++ ScavengerApp/Cells/LocationDetailTitleCell.h
@@ -0,0 +1,15 @@
+//
+//  LocationDetailTitleCell.h
+//  GoHikeAmsterdam
+//
+//  Created by Giovanni on 9/1/13.
+//  Copyright (c) 2013 Code for Europe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationDetailTitleCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *label;
+
+@end
