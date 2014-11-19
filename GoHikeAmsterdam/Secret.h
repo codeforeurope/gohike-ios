e04dd2df717f3c157be07cd0e0e25bc8984c9b09
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-11-19 15:53:11 +0100

    added back empty Secret.h file

---
+++ GoHikeAmsterdam/Secret.h
@@ -0,0 +1,28 @@
+//
+//  Secret.h
+//  ScavengerApp
+//
+//  Created by Giovanni Maggini on 6/4/13.
+//  Copyright (c) 2013 Code for Europe. All rights reserved.
+//
+
+#ifndef GoHikeAmsterdam_Secret_h
+#define GoHikeAmsterdam_Secret_h
+
+//Used for communication with API
+#warning fill API key here
+#define kAPISecret @""
+
+//Used for TestFlight
+#define kTestFlightAPIKey @""
+
+//Used for storing app UUID in keychain and retrieve afterwards
+#define kServiceNameForKeychain @"net.CodeForEurope.GoHikeAmsterdam"
+#define kAccountNameForKeychainDevice @"user"
+#define kAccountNameForKeychainFacebook @"facebook"
+
+//MapBox
+//Use MapBox Map ID here
+#define kMBXMAPID @""
+
+#endif
